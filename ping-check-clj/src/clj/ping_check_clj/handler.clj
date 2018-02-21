7ce2d805e231a49c1b237dbb39b4f4c1adc0659f
ted <itstedpark@gmail.com>
2018-02-21 16:44:34 +0900

    +service
    remove html boilerplate

--- ping-check-clj/src/clj/ping_check_clj/handler.clj
+++ ping-check-clj/src/clj/ping_check_clj/handler.clj
@@ -1,7 +1,5 @@
 (ns ping-check-clj.handler
   (:require [compojure.core :refer [routes wrap-routes]]
-            [ping-check-clj.layout :refer [error-page]]
-            [ping-check-clj.routes.home :refer [home-routes]]
             [ping-check-clj.routes.services :refer [service-routes]]
             [compojure.route :as route]
             [ping-check-clj.env :refer [defaults]]
@@ -16,11 +14,6 @@
   :start
   (middleware/wrap-base
     (routes
-      (-> #'home-routes
-          (wrap-routes middleware/wrap-csrf)
-          (wrap-routes middleware/wrap-formats))
           #'service-routes
       (route/not-found
-        (:body
-          (error-page {:status 404
-                       :title "page not found"}))))))
+        "page not found"))))
